f024257f2cef674a238a028734a16aca0095170a
k906506 <k906506@naver.com>
2021-04-29 10:11:59 +0900

    Uni Test

---
+++ 10 Week/unit-test-tdddemo-master/src/test/java/AccountTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AccountTest {
+
+    //계정 생성 확인 테스트
+
+    //계정 생성자에 초기 입금액 확인 테스트
+
+    //계정에 예금후 잔액 확인 테스트
+
+    //계정에 출금후 잔액 확인 테스트
+
+    //계정에 잔액보다 출금액이 크면 출금되지 않고 잔액이 그대로인지 확인 테스트
+}
